98a015ba4be81c8d2d74e5f588d9389bcc90c5e5
khangle880 <58165933+khangle880@users.noreply.github.com>
2021-05-25 23:32:18 +0700

    add branch

---
+++ src/pages/AddCustomCategory/AddCustomCategory.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class AddCustomCategory extends Component {
+  render() {
+    return <div>abc abc</div>;
+  }
+}
+
+export default AddCustomCategory;
